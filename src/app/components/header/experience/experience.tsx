0f6b3afa3073fde38f00bcc04b14e2f8bee3b05c
gabrielrio2 <gabrielnobreviana2@gmail.com>
2023-09-22 09:44:30 -0300

    Experience bar and mesure

--- src/app/components/header/experience/experience.tsx
+++ src/app/components/header/experience/experience.tsx
@@ -1,12 +1,72 @@
 import { SectionTile } from '../sectionTitle/section-title'
+import Image from 'next/image'
 import './experience.scss'
-export function Experience(){
-    return(
+export function Experience() {
+    return (
         <div className='experience'>
-        <SectionTile text="Experience"/>
-        <p>2 years as a development intern at the company Telecall</p>
-        <div className='experience-time'>
+            <SectionTile text="About" />
+            <p>2 years as a development intern at the company Telecall</p>
+            <div className='experience-time'>
+                <div className='experience-language'>
+                    <Image
 
+                        src="/React-icon.png"
+                        alt="React Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-mesure mesure-1">
+                            <span>1 years</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-language'>
+                    <Image
+
+                        src="/javaScript.png"
+                        alt="JavaScript Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-mesure mesure-1">
+                        <span>1 years</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-language'>
+                    <Image
+
+                        src="/html5.png"
+                        alt="HTML Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-mesure mesure-3">
+                        <span>3 years</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-language'>
+                    <Image
+
+                        src="/C.png"
+                        alt="C# Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-mesure mesure-2">
+                        <span>2 years</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
